9721b22fb1b0bf8b48ec156e6c093fc124f8df0b
BatyLeo <leo.baty67@gmail.com>
2024-10-14 16:01:04 +0200

    portfolio optimization tests

--- test/portfolio_optimization.jl
+++ test/portfolio_optimization.jl
@@ -1,9 +1,29 @@
 @testitem "Portfolio Optimization" begin
     using DecisionFocusedLearningBenchmarks
 
-    b = PortfolioOptimizationBenchmark()
+    d = 50
+    p = 5
+    b = PortfolioOptimizationBenchmark(; d=d, p=p)
 
-    dataset = generate_dataset(b, 100)
+    dataset = generate_dataset(b, 50)
     model = generate_statistical_model(b)
     maximizer = generate_maximizer(b)
+
+    for sample in dataset
+        x = sample.x
+        θ_true = sample.θ
+        y_true = sample.y
+        @test size(x) == (p,)
+        @test length(θ_true) == d
+        @test length(y_true) == d
+        @test isnothing(sample.instance)
+        @test all(y_true .== maximizer(θ_true))
+
+        θ = model(x)
+        @test length(θ) == d
+
+        y = maximizer(θ)
+        @test length(y) == d
+        @test sum(y) <= 1
+    end
 end
