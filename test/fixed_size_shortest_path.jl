29434e7b4a6bc0162f736a1f0cdca1f4e719f840
BatyLeo <leo.baty67@gmail.com>
2024-10-16 15:59:42 +0200

    cleanup and fix fixed size shortest path

--- test/fixed_size_shortest_path.jl
+++ test/fixed_size_shortest_path.jl
@@ -14,15 +14,19 @@
     model = generate_statistical_model(b)
     maximizer = generate_maximizer(b)
 
+    gap = compute_gap(b, dataset, model, maximizer)
+    @test gap >= 0
+
     for sample in dataset
         x = sample.x
         θ_true = sample.θ
         y_true = sample.y
+        @test all(θ_true .< 0)
         @test size(x) == (p,)
         @test length(θ_true) == A
         @test length(y_true) == A
         @test isnothing(sample.instance)
-        @test all(y_true .== maximizer(-θ_true))
+        @test all(y_true .== maximizer(θ_true))
         θ = model(x)
         @test length(θ) == length(θ_true)
         y = maximizer(θ)
