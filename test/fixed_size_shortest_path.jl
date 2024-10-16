fdbc713ef7ae1fc1e8523f2a1e8fb8306ddc8ea0
BatyLeo <leo.baty67@gmail.com>
2024-10-16 15:31:11 +0200

    test fixed size shortest paths

--- test/fixed_size_shortest_path.jl
+++ test/fixed_size_shortest_path.jl
@@ -1,12 +1,31 @@
-module FixedSizeShortestPathTest
+@testitem "FixedSizeShortestPath" begin
+    using DecisionFocusedLearningBenchmarks.FixedSizeShortestPath
+    using Graphs
 
-using DecisionFocusedLearningBenchmarks.FixedSizeShortestPath
+    p = 5
+    grid_size = (5, 5)
+    A = (grid_size[1] - 1) * grid_size[2] + grid_size[1] * (grid_size[2] - 1)
+    b = FixedSizeShortestPathBenchmark(; p=p, grid_size=grid_size)
 
-bench = FixedSizeShortestPathBenchmark()
+    @test nv(b.graph) == grid_size[1] * grid_size[2]
+    @test ne(b.graph) == A
 
-(; features, costs, solutions) = generate_dataset(bench)
+    dataset = generate_dataset(b, 50)
+    model = generate_statistical_model(b)
+    maximizer = generate_maximizer(b)
 
-model = generate_statistical_model(bench)
-maximizer = generate_maximizer(bench)
-
+    for sample in dataset
+        x = sample.x
+        θ_true = sample.θ
+        y_true = sample.y
+        @test size(x) == (p,)
+        @test length(θ_true) == A
+        @test length(y_true) == A
+        @test isnothing(sample.instance)
+        @test all(y_true .== maximizer(-θ_true))
+        θ = model(x)
+        @test length(θ) == length(θ_true)
+        y = maximizer(θ)
+        @test length(y) == length(y_true)
+    end
 end
