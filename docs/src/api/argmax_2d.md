cb688bb50e205dae412dbee95b14e15853becb74
BatyLeo <leo.baty67@gmail.com>
2025-07-16 10:00:33 +0200

    update doc

---
+++ docs/src/api/argmax_2d.md
@@ -0,0 +1,15 @@
+# Argmax2D
+
+## Public
+
+```@autodocs
+Modules = [DecisionFocusedLearningBenchmarks.Argmax2D]
+Private = false
+```
+
+## Private
+
+```@autodocs
+Modules = [DecisionFocusedLearningBenchmarks.Argmax2D]
+Public = false
+```
--- test/argmax_2d.jl
+++ test/argmax_2d.jl
@@ -15,19 +15,19 @@
     for (i, sample) in enumerate(dataset)
         (; x, θ_true, y_true, instance) = sample
         @test length(x) == nb_features
-        @test length(θ_true) == 2  # 2D vectors
-        @test length(y_true) == 2  # 2D point
-        @test !isnothing(sample.instance)  # instance is a polytope
-        @test instance isa Vector{Vector{Float64}}  # polytope is vector of 2D points
-        @test all(length(vertex) == 2 for vertex in instance)  # all vertices are 2D
-        @test y_true in instance  # solution should be a vertex of the polytope
+        @test length(θ_true) == 2
+        @test length(y_true) == 2
+        @test !isnothing(sample.instance)
+        @test instance isa Vector{Vector{Float64}}
+        @test all(length(vertex) == 2 for vertex in instance)
+        @test y_true in instance
         @test y_true == maximizer(θ_true; instance=instance)
 
         θ = model(x)
-        @test length(θ) == 2  # 2D vector
+        @test length(θ) == 2
 
         y = maximizer(θ; instance=instance)
-        @test length(y) == 2  # 2D point
-        @test y in instance  # solution should be a vertex of the polytope
+        @test length(y) == 2
+        @test y in instance
     end
 end
