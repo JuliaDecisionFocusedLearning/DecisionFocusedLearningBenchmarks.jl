522a91f1cabe9ad0f53b836b41025c18618640d8
BatyLeo <leo.baty67@gmail.com>
2025-08-21 16:53:37 +0200

    basic tests for dynamic assortment

---
+++ test/dynamic_assortment.jl
@@ -0,0 +1,25 @@
+@testitem "DVSP" begin
+    using DecisionFocusedLearningBenchmarks
+    using Statistics: mean
+
+    b = DynamicAssortmentBenchmark()
+
+    @test is_endogenous(b)
+    @test !is_exogenous(b)
+
+    dataset = generate_dataset(b, 10; seed=0)
+    environments = generate_environments(b, dataset)
+
+    env = environments[1]
+    get_seed(env)
+    env.seed
+
+    policies = generate_policies(b)
+    expert = policies[1]
+    greedy = policies[2]
+
+    r_expert, _ = evaluate_policy!(expert, environments)
+    r_greedy, _ = evaluate_policy!(greedy, environments)
+
+    @test mean(r_expert) >= mean(r_greedy)
+end
